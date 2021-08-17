25fc3d4bf59e5a81c3927976edf1fb778d7c4102
vitor1255 <vitorhmariano9@gmail.com>
2021-08-17 14:21:29 -0300

    Initial commit

---
+++ src/teste.kt
@@ -0,0 +1,17 @@
+package src
+
+class teste {
+    var nome: String = "Vitor"
+    var cpf: String = "111.111.111-20"
+    private set
+
+    constructor()
+
+    fun pessoainfo() = "$nome e $cpf"
+}
+
+fun main() {
+    val vitor = teste()
+
+    println(vitor.pessoainfo())
+}
