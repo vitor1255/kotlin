58b59d9d39f7f65797ee11dce857a69287da5221
vitor1255 <vitorhmariano9@gmail.com>
2021-08-17 14:33:55 -0300

    teste3

---
+++ src/Conta.kt
@@ -0,0 +1,16 @@
+package src
+
+import java.math.BigDecimal
+
+class Conta(
+    val agencia: String,
+    val numero: String,
+    val saldo: BigDecimal
+) {
+    fun deposito(valor: BigDecimal){
+
+    }
+    fun saque(valor: BigDecimal) {
+
+    }
+}
